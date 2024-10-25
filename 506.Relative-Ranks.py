3dd928f9ac23b3cf6882c8c5e45c6a1f5d505217
Adithya Nayak <153359668+AdithyaNayak18@users.noreply.github.com>
2024-10-25 19:57:23 +0530

    Create 506.Relative-Ranks.py

---
+++ 506.Relative-Ranks.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sc=sorted(score,reverse=True)
+        count={}   #dictionary
+        for i in range(len(sc)):
+            if i==0:
+                count[sc[i]]="Gold Medal"
+            elif i==1:
+                count[sc[i]]="Silver Medal"
+            elif i==2:
+                count[sc[i]]="Bronze Medal"
+            else:
+                count[sc[i]]=str(i+1)
+
+        res=[]
+        for i in score:
+            res.append(count[i])
+
+        return res
+
+
+
+            
+
+        
