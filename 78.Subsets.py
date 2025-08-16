19af5f7e123a95b98b9f1d835a86d678c129b33f
Adithya Nayak <153359668+AdithyaNayak18@users.noreply.github.com>
2025-08-16 19:11:46 +0530

    Update 78.Subsets.py

--- 78.Subsets.py
+++ 78.Subsets.py
@@ -1,3 +1,4 @@
+#Greedy solution
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res=[]
@@ -12,4 +13,19 @@ class Solution:
             sub.pop()
             dfs(i+1)
         dfs(0)
+        return res
+
+
+#Bit Manipulation Solution
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        n=len(nums)
+        ts=1<<n
+        for num in range(0,ts):
+            lst=[]
+            for i in range(0,n):
+                if num&(1<<i)!=0:
+                    lst.append(nums[i])
+            res.append(lst)
         return res
