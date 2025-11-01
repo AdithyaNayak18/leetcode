8cc52b5fcd00c3e475abb172a7283c84dc2c339c
Adithya Nayak <153359668+AdithyaNayak18@users.noreply.github.com>
2025-11-01 23:46:30 +0530

    Implement function to validate binary search tree

---
+++ 98.Validate-Binary-Search-Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        arr=[]
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            arr.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        res=True
+        for i in range(len(arr) - 1):
+            if arr[i] >= arr[i + 1]:
+                res = False
+                break  
+
+        return res
+        
